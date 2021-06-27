0b598cd64a93bdc769ffd67f7e2c0d5573d805c6
Pirate Caveman <piratecaveman@protonmail.com>
2021-06-27 11:46:29 +0530

    picom fixed shadow bspwmrc to use startup script now

---
+++ yarra-valley-rice/bspwm/.config/bspwm/startup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pgrep -x picom >/dev/null || picom --experimental-backends -bc &
+disown -h
+pgrep -x polkit-kde-auth >/dev/null || /usr/lib/polkit-kde-authentication-agent-1 &
+disown -h
+pgrep -x dunst >/dev/null || dunst &
+disown -h
+pgrep -f "${HOME}"/.node_modules/bin/gt >/dev/null || "${HOME}"/.node_modules/bin/gt --dns-type "tls" --dns-server "8622a7.dns.nextdns.io" &
+disown -h
