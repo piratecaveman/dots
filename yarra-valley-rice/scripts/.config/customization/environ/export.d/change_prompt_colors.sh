e19d4d8bec27137de3fd77dce78e396831b16b86
Pirate Caveman <piratecaveman@protonmail.com>
2021-06-30 17:17:55 +0530

    added catfetch.sh new prompt screenshots now omit cursor and add a 1s delay

--- yarra-valley-rice/scripts/.config/customization/environ/export.d/change_prompt_colors.sh
+++ yarra-valley-rice/scripts/.config/customization/environ/export.d/change_prompt_colors.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 RED="\[$(tput setaf 1)\]"
 GREEN="\[$(tput setaf 2)\]"
 YELLOW="\[$(tput setaf 3)\]"
@@ -7,5 +8,5 @@ AQUA="\[$(tput setaf 6)\]"
 WHITE="\[$(tput setaf 7)\]"
 RESET="\[$(tput sgr0)\]"
 
-export PS1="${RED}[${AQUA}\u${WHITE}@${VIOLET}\h ${GREEN}\W${RED}]${WHITE}\$${RESET} "
-
+# export PS1="${RED}[${AQUA}\u${WHITE}@${VIOLET}\h ${GREEN}\W${RED}]${WHITE}\$${RESET} "
+export PS1="${RED} ${RESET}${AQUA} ${RESET}${GREEN} ${RESET}${RED}${BOLD} ${RESET} ${GREEN}\W${RESET} \$ "
