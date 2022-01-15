d7d4e916db2768816ed1f89ae1e52aebf185750b
Pirate Caveman <piratecaveman@protonmail.com>
2022-01-15 12:59:32 +0530

    separated configurations from ricing [WIP]

--- yarra-valley-rice/local-scripts/.local/bin/dyndns.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-curl "https://freemyip.com/update?token=c4573c75203bb73f0e147357&domain=blueringedoctopus.freemyip.com&verbose=yes"
--- yarra-valley-rice/local-scripts/.local/bin/nextdns.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-curl "https://link-ip.nextdns.io/8622a7/799ba88802efdde4"
